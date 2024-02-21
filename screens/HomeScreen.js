daa3819407b297a267316733c94bd4bbc4dc684e
Joseph Ampfer <joseph.ampfer@gmail.com>
2024-02-21 13:36:00 -0500

    Arrows and modal 0.0.1

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import { View, Text, Image, ScrollView } from 'react-native';
-import React from 'react';
+import { View, Text, Image, ScrollView, TouchableOpacity, Modal } from 'react-native';
+import React, { useState } from 'react';
 import tw from 'twrnc';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import LottieView from 'lottie-react-native';
 
 const data = [
   {
-    name: 'Test test 1',
+    name: 'John Doe1',
     time: '500',
     pic: 'https://i.pravatar.cc/600/'
   },
@@ -98,27 +99,30 @@ const data = [
 ]
 
 export default function HomeScreen() {
+  const [visible, setVisible] = useState(false);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
 
   return (
     <View style={tw`flex-1`}>
       <StatusBar style='light' />
       <Image blurRadius={70} source={require('../assets/images/full.png')} style={tw`w-full h-full absolute`} />
       <SafeAreaView style={tw`flex-1`}>
+
         {/* current winner */}
         <View style={[tw`mx-5 h-29 flex rounded-3xl justify-center`, {backgroundColor: 'rgba(255,255,255,0.2)'}]}>
           <View style={tw``}>
             <View style={tw`flex-row justify-between`} >
               {/* pic and stats */}
-              <View style={tw`mx-5`}>
+              <View style={tw`mx-10`}>
                 <Image 
                   source={{ uri: data[0].pic }} 
                   style={tw`h-16 w-16 rounded-full mx-auto`}
                 />
                 <Text style={tw`text-slate-50 font-semibold text-center py-1`}>{data[0].name}</Text>
               </View>
-              <View style={tw`flex-2 justify-center`}>
-                <Text style={tw`font-bold text-lg text-white mr-5 `}> Winning with {data[0].time} </Text>
-                <Text style={tw`font-bold text-lg text-white mr-5 `}> points, catch up!</Text>
+              <View style={tw`flex-2 justify-center `}>
+                <Text style={tw`font-bold text-lg text-white mr-5 text-center`}>{data[0].name} is winning with {data[0].time} points, catch up! </Text>
               </View>
             </View>
           </View>
@@ -169,8 +173,8 @@ export default function HomeScreen() {
                   <Text style={tw`font-bold text-white/80 text-6xl ml-2 mt-2`}>10</Text>
                 </View>
                 <View>
-                  <Text style={tw`flex text-wrap font-semibold text-white/50 ml-2 text-lg`}>hours</Text>
-                  <Text style={tw`flex text-wrap font-semibold text-white/50 ml-2 mr-2 text-left`}>remaining</Text>
+                  <Text style={tw`flex  font-semibold text-white/50 ml-2 text-lg`}>hours</Text>
+                  <Text style={tw`flex  font-semibold text-white/50 ml-2 mr-2 text-left`}>remaining</Text>
                 </View>
               </View>
             </View>
@@ -178,12 +182,54 @@ export default function HomeScreen() {
 
           {/* notify loser */}
           <View style={[tw`mx-5 mt-5 h-19 flex rounded-3xl justify-center`, {backgroundColor: 'rgba(255,255,255,0.2)'}]}>
-
+            <TouchableOpacity 
+              style={tw`w-full h-full flex-row justify-between items-center`}
+              onPress={show}
+            >
+            <View style={tw`h-15 w-15`}>
+                <LottieView 
+                    source={require('../assets/animations/arrows.json')} 
+                    style={{width:'100%', height:'100%', transform: [{rotate: '-90deg'}]}}
+                    autoPlay 
+                    loop 
+                    speed={1}
+                />
+              </View>
+              <View style={tw`justify-center`}>
+                <Text style={tw`text-center  text-white/90 text-sm font-semibold`}>Tell last place they need to touch grass.</Text>
+              </View>
+              <View style={tw`h-15 w-15 `}>
+                <LottieView 
+                    source={require('../assets/animations/arrows.json')} 
+                    style={{width:'100%', height:'100%', transform: [{rotate: '90deg'}]}}
+                    autoPlay 
+                    loop 
+                    speed={1}
+                />
+              </View>
+            </TouchableOpacity>
           </View>
           <View style={[tw`mx-5 mt-5 h-19 flex rounded-3xl justify-center`, {backgroundColor: 'rgba(255,255,255,0.2)'}]}>
-
+            
           </View>
-
+          {/* modal */}
+          <Modal
+            visible={visible}
+            animationType='slide'
+            onRequestClose={hide}
+            transparent={false}
+          >
+            <View style={tw`h-10 w-full`}>
+              <View>
+                <Text>You need to touch grass.</Text>
+              </View>
+              <TouchableOpacity
+                onPress={hide}
+              >
+                <Text>hide</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
 
       </SafeAreaView>
     </View>
